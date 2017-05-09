3e40f498071e3fe00b96179eb567659132039557
Ben Stahl <bhstahl@gmail.com>
2017-05-09 17:14:13 -0400

    Ingore analytics in dev

--- src/static/js/analytics.js
+++ src/static/js/analytics.js
@@ -1,5 +1,17 @@
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WNTNSBS');
+(function(w, d, s, l, i) {
+  if (process.env.ENV === 'dev') {
+      return;
+  }
+
+  w[l] = w[l] || [];
+  w[l].push({
+    "gtm.start": new Date().getTime(),
+    event: "gtm.js"
+  });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != "dataLayer" ? "&l=" + l : "";
+  j.async = true;
+  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, "script", "dataLayer", "GTM-WNTNSBS");
