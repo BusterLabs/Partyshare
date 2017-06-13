c422a48644abab02726b7d1508a2f29ac42d1fc4
Ben Stahl <bhstahl@gmail.com>
2017-06-13 08:11:53 -0400

    Use the filter function

--- src/electron/functions.js
+++ src/electron/functions.js
@@ -2,11 +2,21 @@ const { basename } = require('path');
 const fse = require('fs-extra');
 const fs = require('fs');
 
-const filterHiddenFiles = (item) => {
-    const base = basename(item);
-    return base === '.' || base[0] !== '.';
+/**
+ * Filter function for ignoring hidden files.
+ * @param {String} fileName
+ * @return {boolean}
+ */
+const filterHiddenFiles = (fileName) => {
+    const base = basename(fileName);
+    return !base.startsWith('.');
 };
 
+/**
+ * Ensure a directory exists, and list the contents.
+ * @param {String} dirPath
+ * @return {Promise}
+ */
 const getFiles = (dirPath) => {
     return new Promise((resolve, reject) => {
         fse.ensureDir(dirPath, (err) => {
@@ -18,8 +28,8 @@ const getFiles = (dirPath) => {
                 if (err) {
                     return reject(err);
                 }
-                // Remove hidden files
-                files = files.filter((file) => file.startsWith('.') === false);
+
+                files = files.filter(filterHiddenFiles);
                 return resolve(files);
             });
         });
