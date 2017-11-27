7b1eade2bae3f9c938ded8a882e7a81563c57fc6
Ben Stahl <bhstahl@gmail.com>
2017-11-27 06:45:34 -0500

    Add download button and link to linux builds
    Closes #9

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function handleResponse(res) {
-    var asset = res.assets.find(function(asset) { return asset.content_type === 'application/x-apple-diskimage'});
+if (!String.prototype.endsWith)
+String.prototype.endsWith = function(searchStr, Position) {
+    // This works much better than >= because
+    // it compensates for NaN:
+    if (!(Position < this.length))
+      Position = this.length;
+    else
+      Position |= 0; // round position
+    return this.substr(Position - searchStr.length,
+                       searchStr.length) === searchStr;
+};
+
+function setDownloadLinkToSelector(selector, link) {
+    var downloadButtonEl = document.querySelector(selector);
+    downloadButtonEl.setAttribute('download', true);
+    downloadButtonEl.setAttribute('href', link);
+    downloadButtonEl.removeAttribute('disabled');
+}
+
+function findAndAppendAssetUrlToSelector(assets, extension, selector) {
+    var asset = assets.find(function(asset) { return asset.name.endsWith(extension)});
     if (!asset) {
         return;
     }
-    var downloadButtonEl = document.querySelector('.button--download');
-    downloadButtonEl.setAttribute('download', true);
-    downloadButtonEl.setAttribute('href', asset.browser_download_url);
+
+    setDownloadLinkToSelector(selector, asset.browser_download_url)
+}
+
+function handleResponse(res) {
+    findAndAppendAssetUrlToSelector(res.assets, '.AppImage', '.js-download-mac');
+    findAndAppendAssetUrlToSelector(res.assets, '.dmg', '.js-download-linux');
 }
 
 function fetchDownloadLink() {
